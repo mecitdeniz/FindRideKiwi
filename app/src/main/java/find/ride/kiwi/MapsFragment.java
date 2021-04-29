7f14fe7b22780623e30e25707080de02df2e8d78
mecitdeniz <mecitdenz@gmail.com>
2021-04-30 01:11:21 +0300

    Initial Commit
    FindRideKiwi Project Created in order to create an app that helps users to find a kiwi near to them using google maps

---
+++ app/src/main/java/find/ride/kiwi/MapsFragment.java
@@ -0,0 +1,34 @@
+package find.ride.kiwi;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class MapsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_maps, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(MapsFragment.this)
+                        .navigate(R.id.action_MapsFragment_to_HomeFragment);
+            }
+        });
+    }
+}
