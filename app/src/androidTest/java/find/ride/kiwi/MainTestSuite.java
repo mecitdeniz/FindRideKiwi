6d2170a2f41d0fe4c37aaad5333858a8459db82b
mecitdeniz <mecitdenz@gmail.com>
2021-05-01 16:11:32 +0300

    Testsuite
    TestSuite class added. By using this class we can run all test cases at once

---
+++ app/src/androidTest/java/find/ride/kiwi/MainTestSuite.java
@@ -0,0 +1,9 @@
+package find.ride.kiwi;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({MainActivityTest.class,HomeFragmentTest.class,NavigationTest.class})
+public class MainTestSuite {
+}
