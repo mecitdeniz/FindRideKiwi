d4aa2e951acbdaef4e87bfcf7bdd8b9a94ad9f28
mecitdeniz <mecitdenz@gmail.com>
2021-05-01 15:15:17 +0300

    Add NavigationTest
    Test class added in order to test navigation between fragments

---
+++ app/src/androidTest/java/find/ride/kiwi/NavigationTest.java
@@ -0,0 +1,36 @@
+package find.ride.kiwi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import androidx.test.core.app.ActivityScenario;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.core.app.ActivityScenario.launch;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class NavigationTest {
+
+    @Test
+    public void testNavigationToMapsFragment(){
+        ActivityScenario<MainActivity> activityScenario = launch(MainActivity.class);
+        onView(withId(R.id.button_first)).perform(click());
+
+        onView(withId(R.id.MapsFragment)).check(matches(isDisplayed()));
+    }
+
+
+    @Test
+    public void testBackPressOnMapsFragment(){
+        ActivityScenario<MainActivity> activityScenario = launch(MainActivity.class);
+        onView(withId(R.id.button_first)).perform(click());
+
+        onView(withId(R.id.MapsFragment)).check(matches(isDisplayed()));
+        pressBack();
+        onView(withId(R.id.HomeFragment)).check(matches(isDisplayed()));
+    }
+}
